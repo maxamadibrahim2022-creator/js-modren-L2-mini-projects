f5b6861af36633d7b23d825e95ea64e1eb4c2b1f
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-09-10 08:34:34 +0300

    Small practice

---
+++ 1. Module Arrays/c.💪 Small Practice/main.js
@@ -0,0 +1,19 @@
+// 💪 Small Practice
+    // Try predicting what this produces before running it:
+
+    
+const task = [];
+// STEP 1: Add tasks
+task.push("Build a Project");
+task.push("Practice DOM");
+task.push("Practice Arrays");
+
+// STEP 2: Remove the last task
+const removedTask = task.pop();
+
+// STEP 3: Add a new task
+task.push("Hello maxamad");
+
+// STEP 4: Display everything
+console.log("Removed:", removedTask);
+console.log(task);
