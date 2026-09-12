cf3affab849fc09c7007a5893212e6319be1bbef
maxamadibrahim2022 <maxamadibrahim2022gmail.com>
2026-09-12 10:01:27 +0300

    🚀 Student Attendance

---
+++ 2. Array Methods/A. 🚀 Student Attendance (ForEach)/main.js
@@ -0,0 +1,12 @@
+// ========================================
+// STUDENT ATTENDANCE
+// ========================================
+
+// STEP 1: Create a list of students
+const students = ['maxamad', 'cummar', 'farxiya', 'sundus', 'Qammar', 'sayid cali']
+// STEP 2: Go through every student
+students.forEach((student, index)=>{
+    
+    // STEP 3: Display each student with a number
+    console.log(`${index +1 }. ${student} -present`);
+})
--- main.js
+++ main.js
@@ -13,16 +13,52 @@
 // console.log("removed:", removed)
 
 
-const fruits = ["Apple", "Banana", "Orange","canbe", "qare", "liin"];
+// const fruits = ["Apple", "Banana", "Orange","canbe", "qare", "liin"];
 
-// STEP 1: Go to index 1
-// STEP 2: Remove 1 item
-// STEP 3: Put Mango in its place
-console.log("fruits:", fruits);
+// // STEP 1: Go to index 1
+// // STEP 2: Remove 1 item
+// // STEP 3: Put Mango in its place
+// console.log("fruits:", fruits);
 
- const removed= fruits.splice(1, 3,);
+//  const removed= fruits.splice(1, 3,);
 
-console.log("removed:", removed);
-console.log("newFruits:", fruits);
+// console.log("removed:", removed);
+// console.log("newFruits:", fruits);
+
+// const students = [
+//     "Ali",
+//     "Ahmed",
+//     "Hassan",
+//     "Fatima"
+// ];
+
+// // STEP 1: Go through every student
+// students.forEach((student, index) => {
+
+//     // STEP 2: Display a numbered student
+//     console.log(`${index + 1  }. ${student}`);
+
+// });
 
 
+const products = [
+    {
+        name : "laptop",
+        price : 200,
+        paid : 180
+    },
+    {
+        name : "mouse",
+        price : 2.5,
+        paid : 2.5
+    },
+    {
+        name : "charge",
+        price : 8,
+        paid : 5
+    }
+]
+
+products.forEach((product, index) =>{
+    console.log(`${index +1}. ${product.name} : $${product.price} piad : $${product.paid}`)
+})
